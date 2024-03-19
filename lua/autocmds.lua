630eded181b0e1585773735be57c338fe5954b29
404Gh0st <60356707+404GH0ST@users.noreply.github.com>
2024-03-19 13:57:56 +0700

    feat!: replace none-ls linting with nvim-lint

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,14 +1,23 @@
 require("nvchad.autocmds")
 
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
 -- Auto enable Inlay_hint
 autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	group = augroup("UserLspConfig", {}),
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client.server_capabilities.inlayHintProvider then
 			vim.lsp.inlay_hint.enable(args.buf, true)
 		end
+	end,
+})
+
+local lint_augroup = augroup("lint", { clear = true })
+autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	group = lint_augroup,
+	callback = function()
+		require("lint").try_lint()
 	end,
 })
