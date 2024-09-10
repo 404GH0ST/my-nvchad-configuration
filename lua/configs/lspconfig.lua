b4288ddc813a976fd05889d13b4f0cbfccd13599
Agus Prasetyo <60356707+404GH0ST@users.noreply.github.com>
2024-09-10 23:32:06 +0700

    fix: rename tsserver to ts_ls (#21)

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -101,7 +101,7 @@ lspconfig.gopls.setup({
 	},
 })
 
-lspconfig.tsserver.setup({
+lspconfig.ts_ls.setup({
 	on_attach = function(client, bufnr)
 		client.server_capabilities.documentFormattingProvider = false
 		on_attach(client, bufnr)
